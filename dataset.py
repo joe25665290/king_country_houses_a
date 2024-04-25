8916bb78abbd46927a3dd11d69ec122e2f37d5c4
Zero <zero920829@gmail.com>
2024-04-25 21:31:11 +0800

    fix

--- dataset.py
+++ dataset.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 
 def z_score_normalization(df, cols):
     df_normalized = df.copy()
@@ -19,55 +18,29 @@ def load_data():
     # 讀取 CSV 檔案
     df = pd.read_csv('king_ country_ houses_aa.csv')
 
-
-    feature = df.drop(['price'], axis=1)
+    # 將 'X' 和 'y' 設定為你的特徵和目標變量
+    data = df[[
+        'bedrooms',
+        'bathrooms',
+        'sqft_living',
+        'sqft_lot',
+        'floors',
+        'waterfront',
+        'view',
+        'condition',
+        'grade',
+        'sqft_basement',
+        'yr_built',
+        'yr_renovated',
+        'lat',
+        'long',]]
     target = df['price']
 
-    X_train, X_test, y_train, y_test = train_test_split(feature, target, test_size=0.3, random_state=1)
-
-    # 將 'X' 和 'y' 設定為你的特徵和目標變量
-    trainData = X_train[[
-        'bedrooms',
-        'bathrooms',
-        'sqft_living',
-        'sqft_lot',
-        'floors',
-        'waterfront',
-        'view',
-        'condition',
-        'grade',
-        'sqft_basement',
-        'yr_built',
-        'yr_renovated',
-        'lat',
-        'long',]]
-    TrainTarget = y_train
-
-    testData = X_test[[
-        'bedrooms',
-        'bathrooms',
-        'sqft_living',
-        'sqft_lot',
-        'floors',
-        'waterfront',
-        'view',
-        'condition',
-        'grade',
-        'sqft_basement',
-        'yr_built',
-        'yr_renovated',
-        'lat',
-        'long',]]
-    testTarget = y_test
-
-    trainNormalized = pd.DataFrame(z_score_normalization(trainData, ['yr_built', 'yr_renovated', 'lat', 'long']))
-    testNormalized = pd.DataFrame(z_score_normalization(testData, ['yr_built', 'yr_renovated', 'lat', 'long']))
+    normalized = pd.DataFrame(z_score_normalization(data, ['yr_built', 'yr_renovated', 'lat', 'long']))
     
-    trainData.drop(['yr_built', 'yr_renovated', 'lat', 'long'], axis=1)
-    testData.drop(['yr_built', 'yr_renovated', 'lat', 'long'], axis=1)
+    data.drop(['yr_built', 'yr_renovated', 'lat', 'long'], axis=1)
 
-    trainData = pd.merge(trainData, trainNormalized, left_index=True, right_index=True)
-    testData = pd.merge(testData, testNormalized, left_index=True, right_index=True)
+    data = pd.merge(data, normalized, left_index=True, right_index=True)
 
     return data, target
 
