2b3a4d4f8f13ab93f019ddf2cea99807f8572c72
Terry2999 <terry.hsu321@gmail.com>
2024-04-25 20:52:15 +0800

    merge Zero

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
@@ -68,4 +69,26 @@ def load_data():
     trainData = pd.merge(trainData, trainNormalized, left_index=True, right_index=True)
     testData = pd.merge(testData, testNormalized, left_index=True, right_index=True)
 
-    return trainData, TrainTarget, testData, testTarget
+    return data, target
+
+def data_split(X, y):
+    # 劃分訓練集和測試集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+    # save to csv
+    X_train.to_csv('X_train.csv', index=False)
+    X_test.to_csv('X_test.csv', index=False)
+    y_train.to_csv('y_train.csv', index=False)
+    y_test.to_csv('y_test.csv', index=False)
+
+if __name__ == '__main__':
+    X, y = load_data()
+    data_split(X, y)
+    print('Done!')
+
+def get_data():
+    # return X_train, X_test, y_train, y_test
+    X_train = pd.read_csv('X_train.csv')
+    X_test = pd.read_csv('X_test.csv')
+    y_train = pd.read_csv('y_train.csv')
+    y_test = pd.read_csv('y_test.csv')
+    return X_train, X_test, y_train, y_test
